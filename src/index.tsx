079aa39dff92ad25625133cb9560213934ee799a
Gunesh Asatkar <gunesh.asatkar@numerator.com>
2023-01-23 19:22:29 +0530

    Feature Thumbnail Image Slider

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export * from './components/composite/CustomSearch/index';
 export * from './components/core/Button/index';
 export * from './components/composite/Drawer/index';
 export * from './components/composite/CreativePlayer/index';
+export * from './components/composite/Carousel/index';
+
